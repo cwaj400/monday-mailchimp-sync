d892507017bffd2dea0a188d10a06c1cbfb2489a
cwaj400 <angelljamesw@gmail.com>
2025-03-12 23:21:57 +0000

    added crypto

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
 const { sendDiscordNotification } = require('../utils/discordNotifier');
 const { handleSubscriberEvent } = require('./webhookHandlers/handleSubscriberEvent.js');
 const { handleCampaignEvent } = require('./webhookHandlers/handleCampaignEvent.js');
